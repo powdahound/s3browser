5d6fce62c925ba55f2db555498fdcf1ef33acfc9
Garret Heaton <powdahound@gmail.com>
2008-09-15 14:56:53 +0000

    Install information and better details in sample config

--- config-sample.php
+++ config-sample.php
@@ -2,25 +2,36 @@
 
 $c = array();
 
-// Browser config
-$c['base-path'] = null;
-$c['theme'] = 'plain';
-$c['page-title'] = 'My Amazon S3 files';
-$c['torrent-threshold'] = null; // file size in bytes over which to serve files as torrents
+/**********************************
+ * Browser config
+ **********************************/
 
-// S3 config
+// Base path to directory the browser is running in. Leave blank if running out of a subdomain.
+$c['base-path'] = null;
+
+// Name of theme to use for display. Themes are found in the themes/ directory.
+$c['theme'] = 'plain';
+
+// Text to use as page header
+$c['page-title'] = 'My Amazon S3 files';
+
+// File size in bytes over which to serve files as torrents
+$c['torrent-threshold'] = null;
+
+// Amazon S3 access information
 $c['s3-access-key'] = '';
 $c['s3-secret-key'] = '';
 
-// Caching
-$c['cache-time'] = 0;
+// Bucket information should be cached so your S3 account doesn't need to be queried for every user request. Default cache-time is 10 minutes.
+$c['cache-time'] = 60 * 10;
 $c['cache-dir'] = '/tmp';
 
 // Bucket
 $c['bucket-name'] = '';
 $c['bucket-url-prefix'] = 'http://'.$c['bucket-name'].'.s3.amazonaws.com';
 
-// Google analytics
+// Google analytics id to use for tracking
+// Download links will also be recorded in Google Analytics
 $c['google-analytics-id'] = null;
 
 return $c;
