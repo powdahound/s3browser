1774946e8c35ed246a2304a7a5dc43466813403a
Garret Heaton <powdahound@gmail.com>
2010-03-04 20:46:02 -0800

    Make sure config was set properly.
    Also obey 80 char line length

--- index.php
+++ index.php
@@ -9,13 +9,19 @@ if (!file_exists($configFile)) {
 }
 $config = include($configFile);
 
-// S3Browser
-$s3b = new S3Browser($config['bucket-name'], $config['s3-access-key'], $config['s3-secret-key']);
+if (!$config['bucket-name'] || !$config['s3-access-key'] ||
+    !$config['s3-secret-key']) {
+  die('Please set bucket-name, s3-access-key, and s3-secret-key in'.
+      ' config.php');
+}
+
+$s3b = new S3Browser($config['bucket-name'], $config['s3-access-key'],
+                     $config['s3-secret-key']);
 $s3b->enableCaching($config['cache-dir'], $config['cache-time']);
 
 // Get current directory from URL
-$dir = str_replace($c['base-path'], '', $_SERVER['REQUEST_URI']);
+$dir = str_replace($config['base-path'], '', $_SERVER['REQUEST_URI']);
 
 $files = $s3b->getFiles($dir);
 
-include ROOT_DIR.'/themes/'.$c['theme'].'/index.tpl.php';
+include ROOT_DIR.'/themes/'.$config['theme'].'/index.tpl.php';
