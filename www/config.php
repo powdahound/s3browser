05e42cdc3ed0b4aa447b427bf55f1f96bc085410
Garret Heaton <powdahound@gmail.com>
2015-01-11 15:13:24 -0800

    Pull all other config from env. Working well

--- www/config.php
+++ www/config.php
@@ -2,32 +2,32 @@
 
 $c = array();
 
-// Base path to directory the browser is running in. Leave blank if running out of a subdomain.
-$c['base-path'] = null;
+// Base path to directory the browser is running in. Leave blank if running out of a subdomain (like on Heroku)
+$c['base-path'] = getenv('BASE_PATH') || null;
 
 // Name of theme to use for display. Themes are found in the themes/ directory.
-$c['theme'] = 'plain';
+$c['theme'] = getenv('THEME') || 'plain';
 
 // Text to use as page header
-$c['page-header'] = 'My Amazon S3 files';
+$c['page-header'] = getenv('PAGE_HEADER') || 'My Amazon S3 files';
 
 // File size in bytes over which to serve files as torrents
-$c['torrent-threshold'] = null;
+$c['torrent-threshold'] = getenv('TORRENT_THRESHOLD') || null;
 
 // Amazon S3 access information
 $c['s3-access-key'] = getenv('S3_ACCESS_KEY');
 $c['s3-secret-key'] = getenv('S3_SECRET_KEY');
 
 // Bucket information should be cached so your S3 account doesn't need to be queried for every user request. Default cache-time is 10 minutes.
-$c['cache-time'] = 60 * 10;
-$c['cache-dir'] = '/tmp';
+$c['cache-time'] = getenv('CACHE_TIME') || 60 * 10;
+$c['cache-dir'] = getenv('CACHE_DIR') || '/tmp';
 
 // Bucket
 $c['bucket-name'] = getenv('BUCKET_NAME');
-$c['bucket-url-prefix'] = 'http://'.$c['bucket-name'].'.s3.amazonaws.com';
+$c['bucket-url-prefix'] = getenv('BUCKET_URL_PREFIX') || 'http://'.$c['bucket-name'].'.s3.amazonaws.com';
 
 // Google analytics id to use for tracking
 // Download links will also be recorded in Google Analytics
-$c['google-analytics-id'] = null;
+$c['google-analytics-id'] = getenv('GOOGLE_ANALYTICS_ID') || null;
 
 return $c;
