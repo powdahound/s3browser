a78ae4b81d2e21db2abd5a4615070122d0c615cb
Garret Heaton <powdahound@gmail.com>
2015-01-11 14:47:49 -0800

    Pull config from env

--- www/config.php
+++ www/config.php
@@ -15,15 +15,15 @@ $c['page-header'] = 'My Amazon S3 files';
 $c['torrent-threshold'] = null;
 
 // Amazon S3 access information
-$c['s3-access-key'] = '';
-$c['s3-secret-key'] = '';
+$c['s3-access-key'] = getenv('S3_ACCESS_KEY');
+$c['s3-secret-key'] = getenv('S3_SECRET_KEY');
 
 // Bucket information should be cached so your S3 account doesn't need to be queried for every user request. Default cache-time is 10 minutes.
 $c['cache-time'] = 60 * 10;
 $c['cache-dir'] = '/tmp';
 
 // Bucket
-$c['bucket-name'] = '';
+$c['bucket-name'] = getenv('BUCKET_NAME');
 $c['bucket-url-prefix'] = 'http://'.$c['bucket-name'].'.s3.amazonaws.com';
 
 // Google analytics id to use for tracking
